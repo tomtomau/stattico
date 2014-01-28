152f9f0e3786333ac33fb275f7ca07c6ba77567c
tomtomau <tom.newby.au@gmail.com>
2014-01-28 23:51:04 +1000

    Added index!

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<head>
+<title>statti.co strava club analytics</title>
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">	
+</head>
+<body>
+<div class="container">
+<h1>statti.co<br><small>strava club analytics for redditors</small></h1>
+<div class="jumbotron">
+	<p>This web app is a tool written by a fellow cyclist and redditor to do basic 
+	data summaries on the reddit clubs on strava, <a href="http://strava.com/clubs/165">/r/bicycling</a> 
+	and <a href="http://strava.com/clubs/25148">/r/bikecommuting</a>. It collects strava data on a regular basis, but
+	it might not be perfect. The project is available open source on <a href="https://github.com/tomtomau/stattico">GitHub</a>!
+	</p>
+</div>
+<h1>View Data Summaries</h1>
+<div class="container">
+	<hr>
+<div>
+	<h3><a href="bicycling/">/r/bicycling</a></h3>
+</div>
+<hr>
+<div>
+	<h3><a href="bikecommuting/">/r/bikecommuting</a></h3>
+</div>
+<hr>
+</div>
+<div>
+	<p>Built with <a href="http://getbootstrap.com">Bootstrap</a> by Tom Newby (<a href="http://reddit.com/u/tomtomau">/u/tomtomau</a>) - available on <a href="https://github.com/tomtomau/stattico">GitHub</a></p>
+	</div>
+</div>
+</body>
+</html>
